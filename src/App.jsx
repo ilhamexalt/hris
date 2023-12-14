38268811eded65b1666d366b0734aa0c8411f133
ilhamexalt <wmeluck24@gmail.com>
2023-12-14 20:26:20 +0700

    update default path

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/forgotpassword" element={<ForgotPassword />} />
         </Routes>
